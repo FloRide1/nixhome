f5276b00b3da626bfe93ec1e4dab5d59b699b8eb
FloRide1 <florian.reimat@orange.fr>
2021-10-22 18:28:27 +0200

    Reformat nvim

--- programs/nvim/default.nix
+++ programs/nvim/default.nix
@@ -1,269 +1,264 @@
 { pkgs, ... }:
 
 {
-    programs.neovim = {
-        enable = true;
-        vimAlias = true;
-        extraConfig = ''
+	programs.neovim = {
+		enable = true;
+		vimAlias = true;
+		extraConfig = ''
 
-            """"""""""""""""""""""""""""""""""""""""""""""""""
-            " Description:
-            "   This is the .vimrc file
-            "
-            " Maintainer:
-            "	Florian 'FloRide' Reimat
-            "
-            " Complete_version:
-            "   You can find the complete configuration,
-            "   including all the plugins used, here:
-            "   https://github.com/FloRide1/dotfiles
-            "
-            " Acknowledgements:
-            "   Several elements of this .vimrc come from Pierre Bourdon's config
-            "
-            """"""""""""""""""""""""""""""""""""""""""""""""""
+			""""""""""""""""""""""""""""""""""""""""""""""""""
+			" Description:
+			"   This is the .vimrc file
+			"
+			" Maintainer:
+			"	Florian 'FloRide' Reimat
+			"
+			" Complete_version:
+			"   You can find the complete configuration,
+			"   including all the plugins used, here:
+			"   https://github.com/FloRide1/dotfiles
+			"
+			" Acknowledgements:
+			"   Several elements of this .vimrc come from Pierre Bourdon's config
+			"
+			""""""""""""""""""""""""""""""""""""""""""""""""""
 
-            " Enable filetype detection for plugins and indentation options
-            filetype plugin indent on
+			" Enable filetype detection for plugins and indentation options
+			filetype plugin indent on
 
-            " Reload a file when it is changed from the outside
-            set autoread
+			" Reload a file when it is changed from the outside
+			set autoread
 
-            """"""""""""""""""""""""""""""""""""""""""""""""""
-            " User interface
-            """"""""""""""""""""""""""""""""""""""""""""""""""
-            " Set encoding
-            set encoding=utf-8
+			""""""""""""""""""""""""""""""""""""""""""""""""""
+			" User interface
+			""""""""""""""""""""""""""""""""""""""""""""""""""
+			" Set encoding
+			set encoding=utf-8
 
-            " Coc TextEdit might fail if hidden is not set.
-            set hidden
+			" Coc TextEdit might fail if hidden is not set.
+			set hidden
 
-            " Coc Some server have issues with backup files
-            set nobackup
-            set nowritebackup
+			" Coc Some server have issues with backup files
+			set nobackup
+			set nowritebackup
 
-            " Coc Give more space for displaying messages.
-            set cmdheight=2
+			" Coc Give more space for displaying messages.
+			set cmdheight=2
 
-            " Clipboard
-            set clipboard=unnamedplus
+			" Clipboard
+			set clipboard=unnamedplus
 
-            " Coc Having longer update time leads is boring
-            set updatetime=300
+			" Coc Having longer update time leads is boring
+			set updatetime=300
 
-            " Coc don't pass messages to |ins-completion-menu|: [UNKNOW]
-            " set shortmess+=c
+			" Coc don't pass messages to |ins-completion-menu|: [UNKNOW]
+			" set shortmess+=c
 
-            " Make backspace behave as expected 
-            set backspace=eol,indent,start
+			" Make backspace behave as expected 
+			set backspace=eol,indent,start
 
-            " Set the minimal amount of lignes under and above the cursor
-            set scrolloff=5
+			" Set the minimal amount of lignes under and above the cursor
+			set scrolloff=5
 
-            " Show current mode
-            set showmode
+			" Show current mode
+			set showmode
 
-            " Show command being executed
-            set showcmd
+			" Show command being executed
+			set showcmd
 
-            " Show line number
-            set number
+			" Show line number
+			set number
 
-            " Set completion behavior, see :help wildmode for details
-            set wildmode=longest:full,list:full
+			" Set completion behavior, see :help wildmode for details
+			set wildmode=longest:full,list:full
 
-            " Disable bell completely
-            set visualbell
-            set t_vb=
+			" Disable bell completely
+			set visualbell
+			set t_vb=
 
-            " Display whitespace characters
-            set list
-            set listchars=tab:>─,eol:¬,trail:\ ,nbsp:¤
+			" Display whitespace characters
+			set list
+			set listchars=tab:>─,eol:¬,trail:\ ,nbsp:¤
 
-            set fillchars=vert:│
+			set fillchars=vert:│
 
-            " Briefly show matching braces, parens, etc
-            set showmatch
+			" Briefly show matching braces, parens, etc
+			set showmatch
 
-            " Enable line wrapping
-            set wrap
+			" Enable line wrapping
+			set wrap
 
-            " Wrap on column 80
-            set textwidth=79
+			" Wrap on column 80
+			set textwidth=79
 
-            " Disable preview window on completion
-            set completeopt=longest,menuone
+			" Disable preview window on completion
+			set completeopt=longest,menuone
 
-            " Enables syntax highlighting
-            syntax on
+			" Enables syntax highlighting
+			syntax on
 
-            " Highlight current line
-            set cursorline
+			" Highlight current line
+			set cursorline
 
-            " Enhanced indent
-            set smartindent
+			" Enhanced indent
+			set smartindent
 
-            " Add GDB Debugger
-            packadd termdebug
+			" Add GDB Debugger
+			packadd termdebug
 
-            " Gdb Config
-            let g:termdebug_wide=1
+			" Gdb Config
+			let g:termdebug_wide=1
 
-            """"""""""""""""""""""""""""""""""""""""""""""""""
-            " Search options
-            """"""""""""""""""""""""""""""""""""""""""""""""""
+			""""""""""""""""""""""""""""""""""""""""""""""""""
+			" Search options
+			""""""""""""""""""""""""""""""""""""""""""""""""""
 
-            " Ignore case on search
-            set ignorecase
+			" Ignore case on search
+			set ignorecase
 
-            " Ignore case unless there is an uppercase letter in the pattern
-            set smartcase
+			" Ignore case unless there is an uppercase letter in the pattern
+			set smartcase
 
-            " Move cursor to the matched string
-            set incsearch
+			" Move cursor to the matched string
+			set incsearch
 
-            " Don't highlight matched strings
-            set nohlsearch
+			" Don't highlight matched strings
+			set nohlsearch
 
-            """"""""""""""""""""""""""""""""""""""""""""""""""
-            " Indentation options
-            """"""""""""""""""""""""""""""""""""""""""""""""""
+			""""""""""""""""""""""""""""""""""""""""""""""""""
+			" Indentation options
+			""""""""""""""""""""""""""""""""""""""""""""""""""
 
-            " The length of a tab
-            " This is for documentation purposes only,
-            " do not change the default value of 8, ever.
-            set tabstop=4
+			" The length of a tab
+			" This is for documentation purposes only,
+			" do not change the default value of 8, ever.
+			set tabstop=4
 
-            " The number of spaces inserted/removed when using < or >
-            set shiftwidth=4
+			" The number of spaces inserted/removed when using < or >
+			set shiftwidth=4
 
-            " The number of spaces inserted when you press tab.
-            " -1 means the same value as shiftwidth
-            set softtabstop=-1
+			" The number of spaces inserted when you press tab.
+			" -1 means the same value as shiftwidth
+			set softtabstop=-1
 
-            " Insert spaces instead of tabs
-            " set expandtab
+			" Insert spaces instead of tabs
+			" set expandtab
 
-            " When tabbing manually, use shiftwidth instead of tabstop and softtabstop
-            set smarttab
+			" When tabbing manually, use shiftwidth instead of tabstop and softtabstop
+			set smarttab
 
-            " Set basic indenting (i.e. copy the indentation of the previous line)
-            " When filetype detection didn't find a fancy indentation scheme
-            set autoindent
+			" Set basic indenting (i.e. copy the indentation of the previous line)
+			" When filetype detection didn't find a fancy indentation scheme
+			set autoindent
 
-            """"""""""""""""""""""""""""""""""""""""""""""""""
-            " Mappings
-            """"""""""""""""""""""""""""""""""""""""""""""""""
+			""""""""""""""""""""""""""""""""""""""""""""""""""
+			" Mappings
+			""""""""""""""""""""""""""""""""""""""""""""""""""
 
-            " Write as root, when you forgot to sudoedit
-            cnoreabbrev w!! w !sudo tee % >/dev/null
+			" Write as root, when you forgot to sudoedit
+			cnoreabbrev w!! w !sudo tee % >/dev/null
 
-            " Yank from cursor to end of line, to be consistent with C and D
-            nnoremap Y y$
+			" Yank from cursor to end of line, to be consistent with C and D
+			nnoremap Y y$
 
-            " map ; to :
-            noremap ; :
+			" map ; to :
+			noremap ; :
 
-            " Split Screen
-            nmap <silent> <A-Up> :wincmd k<CR>
-            nmap <silent> <A-Down> :wincmd j<CR>
-            nmap <silent> <A-Left> :wincmd h<CR>
-            nmap <silent> <A-Right> :wincmd l<CR>
+			" Split Screen
+			nmap <silent> <A-Up> :wincmd k<CR>
+			nmap <silent> <A-Down> :wincmd j<CR>
+			nmap <silent> <A-Left> :wincmd h<CR>
+			nmap <silent> <A-Right> :wincmd l<CR>
 
-            imap <C-w> <C-o><C-w>
+			imap <C-w> <C-o><C-w>
 
-            map + <C-W>+
-            map - <C-W>-
-            map <M-.> <C-W><
-            map <M-,> <C-W>>
+			map + <C-W>+
+			map - <C-W>-
+			map <M-.> <C-W><
+			map <M-,> <C-W>>
 
-            " Run make silently, then skip the 'Press ENTER to continue'
-            noremap <leader>m :silent! :make! \| :redraw!<cr>
+			" Run make silently, then skip the 'Press ENTER to continue'
+			noremap <leader>m :silent! :make! \| :redraw!<cr>
 
-            " Map ESC to exit terminal mode
-            tnoremap <Esc> <C-\><C-n>
-            nnoremap <silent> <C-b> :Break<CR>
-            nnoremap <silent> <C-c> :Clear<CR>
-            nnoremap <silent> <F5> :Continue<CR>
+			" Map ESC to exit terminal mode
+			tnoremap <Esc> <C-\><C-n>
+			nnoremap <silent> <C-b> :Break<CR>
+			nnoremap <silent> <C-c> :Clear<CR>
+			nnoremap <silent> <F5> :Continue<CR>
 
-            """"""""""""""""""""""""""""""""""""""""""""""""""
-            " Persistence options
-            """"""""""""""""""""""""""""""""""""""""""""""""""
+			""""""""""""""""""""""""""""""""""""""""""""""""""
+			" Persistence options
+			""""""""""""""""""""""""""""""""""""""""""""""""""
 
-            let g:termdebug_popup = 0
-            let g:termdebug_wide = 163
+			let g:termdebug_popup = 0
+			let g:termdebug_wide = 163
 
-            """"""""""""""""""""""""""""""""""""""""""""""""""
-            " Plugin mappings and options
-            """"""""""""""""""""""""""""""""""""""""""""""""""
-            " Ulti Snips
-            let g:UltiSnipsExpandTrigger="<tab>"
-            let g:UltiSnipsJumpForwardTrigger="<tab>"
-            let g:UltiSnipsJumpBackwardTrigger="<c-z>"
-            
-            " Git Gutter
-            let g:gitgutter_sign_added = '✚'
-            let g:gitgutter_sign_modified = '✹'
-            let g:gitgutter_sign_removed = '-'
-            let g:gitgutter_sign_removed_first_line = '-'
-            let g:gitgutter_sign_modified_removed = '-'
+			""""""""""""""""""""""""""""""""""""""""""""""""""
+			" Plugin mappings and options
+			""""""""""""""""""""""""""""""""""""""""""""""""""
+			" Ulti Snips
+			let g:UltiSnipsExpandTrigger="<tab>"
+			let g:UltiSnipsJumpForwardTrigger="<tab>"
+			let g:UltiSnipsJumpBackwardTrigger="<c-z>"
 
-            inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
-            inoremap <C-Space> <C-n>
+			" Git Gutter
+			let g:gitgutter_sign_added = '✚'
+			let g:gitgutter_sign_modified = '✹'
+			let g:gitgutter_sign_removed = '-'
+			let g:gitgutter_sign_removed_first_line = '-'
+			let g:gitgutter_sign_modified_removed = '-'
 
-            " Use <c-space> to trigger completion.
-            "inoremap <silent><expr> <c-space> coc#refresh()
+			inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+			inoremap <C-Space> <C-n>
 
-            " Symbol renaming.
-            nmap <c-r>r <Plug>(coc-rename)
+			" Use <c-space> to trigger completion.
+			"inoremap <silent><expr> <c-space> coc#refresh()
 
-            " Use K to show documentation in preview window.
-            nnoremap <silent> K :call <SID>show_documentation()<CR>
+			" Symbol renaming.
+			nmap <c-r>r <Plug>(coc-rename)
 
-            function! s:show_documentation()
-            if (index(['vim','help'], &filetype) >= 0)
-                execute 'h '.expand('<cword>')
-                    elseif (coc#rpc#ready())
-                    call CocActionAsync('doHover')
-            else
-                execute '!' . &keywordprg . " " . expand('<cword>')
-                    endif
-                    endfunction
+			" Use K to show documentation in preview window.
+			nnoremap <silent> K :call <SID>show_documentation()<CR>
 
-                    " Highlight the symbol and its references when holding the cursor.
-                    autocmd CursorHold * silent call CocActionAsync('highlight')
+			function! s:show_documentation()
+			if (index(['vim','help'], &filetype) >= 0)
+				execute 'h '.expand('<cword>')
+					elseif (coc#rpc#ready())
+					call CocActionAsync('doHover')
+			else
+				execute '!' . &keywordprg . " " . expand('<cword>')
+					endif
+					endfunction
 
-                    set statusline^=%{coc#status()}%{get(b:,'coc_current_function','\')}
-            """"""""""""""""""""""""""""""""""""""""""""""""""
-            " Theme
-            """"""""""""""""""""""""""""""""""""""""""""""""""
-            colorscheme tender
-            let g:airline_powerline_fonts = 1
-            let g:airline_theme = 'tender'
-            '';
+					" Highlight the symbol and its references when holding the cursor.
+					autocmd CursorHold * silent call CocActionAsync('highlight')
+
+					set statusline^=%{coc#status()}%{get(b:,'coc_current_function','\')}
+			""""""""""""""""""""""""""""""""""""""""""""""""""
+			" Theme
+			""""""""""""""""""""""""""""""""""""""""""""""""""
+			colorscheme tender
+			let g:airline_powerline_fonts = 1
+			let g:airline_theme = 'tender'
+			'';
 
 
 
-        plugins = with pkgs.vimPlugins; [ 
-            #vim-polyglot
-            vim-airline
-            vim-airline-themes
-            tender-vim
-            #lightline-vim
-            vim-devicons
-            vim-clang-format
-            vim-gitgutter
-            ultisnips
-            vimtex
-            #ayu-vim
-            #nerdtree
-            #fzf-vim
-            coc-nvim
-            coc-pairs
-            coc-yaml
-            coc-clangd
+		plugins = with pkgs.vimPlugins; [ 
+			vim-airline
+			vim-airline-themes
+			tender-vim
+			vim-devicons
+			vim-clang-format
+			vim-gitgutter
+			ultisnips
+			vimtex
+			coc-nvim
+			coc-pairs
+			coc-yaml
+			coc-clangd
 			which-key-nvim
 			nvim-colorizer-lua
-        ];
-    };
+		];
+	};
 }
