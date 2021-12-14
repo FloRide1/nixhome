f7625621eadc01fd136be8d4cef983c9ce44d979
FloRide1 <florian.reimat@orange.fr>
2021-12-14 13:20:07 +0100

    [GIT] Improve git config

--- programs/git/default.nix
+++ programs/git/default.nix
@@ -7,9 +7,40 @@
 		userEmail = "florian.reimat@orange.fr";
 		extraConfig = {
 			init.defaultBranch = "master";
+			pull.ff = "true";
+			core = {
+				editor = "nvim";
+				pager = "delta --dark";
+			};
 		};
-		delta = {
-			enable = true;
+		aliases =
+		{
+			mr = "!sh -c 'git fetch $1 merge-requests/$2/head:mr-$1-$2 && git checkout mr-$1-$2' -";
+			l = "log --graph --pretty='%Cred%h%Creset - %C(bold blue)<%an>%Creset %s%C(yellow)%d%Creset %Cgreen(%cr)' --abbrev-commit --date=relative";
+			whoops = "reset --hard";
+			back = "reset --hard HEAD^";
 		};
+		ignores = [
+			# Swap
+			"[._]*.s[a-v][a-z]"
+			"[._]*.sw[a-p]"
+			"[._]s[a-rt-v][a-z]"
+			"[._]ss[a-gi-z]"
+			"[._]sw[a-p]"
+
+			# Session
+			"Session.vim"
+			"Sessionx.vim"
+
+			# Temporary
+			".netrwhist"
+			"*~"
+
+			# Auto-generated tag files
+			"tags"
+
+			# Persistent undo
+			"[._]*.un~"
+		];
 	};
 }
