01acd240959eda01355fc900b8ad68fb5bd08cb0
FloRide1 <florian.reimat@orange.fr>
2022-04-29 18:45:36 +0200

    [I3] Edit keybingings

--- services/i3/default.nix
+++ services/i3/default.nix
@@ -36,25 +36,35 @@ in
 
             # Custom workspaces names
             "${modifier}+1" = "workspace ${ws1}";
-            "${modifier}+Shift+1" = "move container to workspace ${ws1}";
             "${modifier}+2" = "workspace ${ws2}";
-            "${modifier}+Shift+2" = "move container to workspace ${ws2}";
             "${modifier}+3" = "workspace ${ws3}";
-            "${modifier}+Shift+3" = "move container to workspace ${ws3}";
             "${modifier}+4" = "workspace ${ws4}";
-            "${modifier}+Shift+4" = "move container to workspace ${ws4}";
             "${modifier}+5" = "workspace ${ws5}";
-            "${modifier}+Shift+5" = "move container to workspace ${ws5}";
             "${modifier}+6" = "workspace ${ws6}";
-            "${modifier}+Shift+6" = "move container to workspace ${ws6}";
             "${modifier}+7" = "workspace ${ws7}";
-            "${modifier}+Shift+7" = "move container to workspace ${ws7}";
             "${modifier}+8" = "workspace ${ws8}";
-            "${modifier}+Shift+8" = "move container to workspace ${ws8}";
             "${modifier}+9" = "workspace ${ws9}";
-            "${modifier}+Shift+9" = "move container to workspace ${ws9}";
             "${modifier}+0" = "workspace ${ws0}";
+
+            "${modifier}+Shift+1" = "move container to workspace ${ws1}";
+            "${modifier}+Shift+2" = "move container to workspace ${ws2}";
+            "${modifier}+Shift+3" = "move container to workspace ${ws3}";
+            "${modifier}+Shift+4" = "move container to workspace ${ws4}";
+            "${modifier}+Shift+5" = "move container to workspace ${ws5}";
+            "${modifier}+Shift+6" = "move container to workspace ${ws6}";
+            "${modifier}+Shift+7" = "move container to workspace ${ws7}";
+            "${modifier}+Shift+8" = "move container to workspace ${ws8}";
+            "${modifier}+Shift+9" = "move container to workspace ${ws9}";
             "${modifier}+Shift+0" = "move container to workspace ${ws0}";
+
+			"${modifier}+b" = "split h";
+			"${modifier}+v" = "split v";
+
+			"${modifier}+h" = "focus left";
+			"${modifier}+j" = "focus down";
+			"${modifier}+k" = "focus up";
+			"${modifier}+l" = "focus right";
+
             # Lock
             "${modifier}+x" = "exec ${pkgs.i3lock-fancy}/bin/i3lock-fancy -p -t \"System Locked Down\"";
         };
