dda9fda2e746edbf6e993d78ef8fd6886fae07f4
FloRide1 <florian.reimat@orange.fr>
2022-04-29 14:52:01 +0200

    [NVIM] Remove lsp-installer (temporarily)
    It's cause crash while home-manager switch

--- programs/nvim/default.nix
+++ programs/nvim/default.nix
@@ -37,7 +37,7 @@ in
 			vim-dadbod
 			vim-dadbod-ui
 			(plugin "neovim/nvim-lspconfig")
-			(plugin "williamboman/nvim-lsp-installer")
+			# (plugin "williamboman/nvim-lsp-installer")
 			(plugin "puremourning/vimspector")
 		];
 
