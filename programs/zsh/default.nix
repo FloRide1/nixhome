6418eeae6ae1bab5abf5d310a0697a1694e658b9
FloRide1 <florian.reimat@orange.fr>
2022-06-04 14:58:02 +0200

    [ZSH] Add plugins for autoswitching in nix-shell

--- programs/zsh/default.nix
+++ programs/zsh/default.nix
@@ -25,6 +25,16 @@
           sha256 = "03r6hpb5fy4yaakqm3lbf4xcvd408r44jgpv4lnzl9asp4sb9qc0";
         };
       }
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.5.0";
+          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+        };
+      }
     ];
 
     oh-my-zsh = {
