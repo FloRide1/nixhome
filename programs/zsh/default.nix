f5125a0b8d5e1e60ffb458c1a8cb5fbdd1d6a982
FloRide1 <florian.reimat@orange.fr>
2022-07-08 17:09:52 +0200

    [ZSH] Add envExtra + syntaxHighlight builtin

--- programs/zsh/default.nix
+++ programs/zsh/default.nix
@@ -5,6 +5,7 @@
     enable = true;
     enableCompletion = true;
     enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
     plugins = [
       {
         name = "powerlevel10k";
@@ -17,15 +18,6 @@
         file = "p10k.zsh";
       }
       {
-        name = "zsh-syntax-highlighting";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-syntax-highlighting";
-          rev = "0.7.1";
-          sha256 = "03r6hpb5fy4yaakqm3lbf4xcvd408r44jgpv4lnzl9asp4sb9qc0";
-        };
-      }
-      {
         name = "zsh-nix-shell";
         file = "nix-shell.plugin.zsh";
         src = pkgs.fetchFromGitHub {
@@ -36,6 +28,8 @@
         };
       }
     ];
+
+    envExtra = "LANG=C.UTF-8";
 
     oh-my-zsh = {
       enable = true;
