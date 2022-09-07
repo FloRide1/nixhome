abbe84d2a7a814456184e243a6ded97fb33db2a2
FloRide1 <florian.reimat@orange.fr>
2022-09-07 11:00:16 +0200

    [HOME] Build nvim dev version instead of packages

--- home.nix
+++ home.nix
@@ -63,6 +63,12 @@ in {
   programs.home-manager.enable = true;
 
   nixpkgs.config = { allowUnfree = true; };
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url =
+        "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
+    }))
+  ];
 
   home = {
     username = "floride";
