bab217368dd116ab3107c547184dc51568eda3c1
FloRide1 <florian.reimat@orange.fr>
2023-02-14 03:25:15 +0100

    [FISH] Add plugins + missings shortcuts

--- programs/fish/default.nix
+++ programs/fish/default.nix
@@ -4,20 +4,41 @@
   programs.fish = {
     enable = true;
 
-    plugins = [{
-      name = "tide";
-      src = pkgs.fishPlugins.tide.src;
-    }];
+    plugins = [
+      {
+        name = "tide";
+        src = pkgs.fishPlugins.tide.src;
+      }
+
+      {
+        name = "sponge";
+        src = pkgs.fishPlugins.sponge.src;
+      }
+
+      {
+        name = "grc";
+        src = pkgs.fishPlugins.grc.src;
+      }
+
+      {
+        name = "colored-man-pages";
+        src = pkgs.fishPlugins.colored-man-pages.src;
+      }
+
+    ];
 
     shellAliases = {
       v = "nvim";
       c = "clear";
-      unziptar = "tar -xvzf";
+      untar = "tar -xvzf";
+      tarc = "tar -cvzf";
       mkdir = "mkdir -vp";
       rm = "rm -riv";
       cp = "cp -riv";
+      mv = "mv -iv";
       cat = "bat -p";
       ls = "lsd";
+      sl = "lsd";
       tree = "lsd --tree";
     };
   };
