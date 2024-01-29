5c9ac43a72968c46ad3fd98c4f0365d24a9cf053
FloRide1 <florian.reimat@orange.fr>
2024-01-29 17:48:11 +0100

    [WAYLAND] Add Electron env variable

--- services/wayland/default.nix
+++ services/wayland/default.nix
@@ -28,6 +28,7 @@
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     SDL_VIDEODRIVER = "wayland";
     XDG_SESSION_TYPE = "wayland";
+    NIXOS_OZONE_WL = "1";
   };
 
   # fake a tray to let apps start
